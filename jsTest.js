a80c074314c8cb4391b33dfcc2c6f4a70d0ced83
jchalaya <julifanny4@gmail.com>
2016-03-04 19:56:01 +0200

    Update jsTest.js

--- jsTest.js
+++ jsTest.js
@@ -1,25 +1,22 @@
 document.getElementById("result").onclick= function checkTest() {
 	var percent = 0;
     var ball = 0;
-	var ans1 = document.getElementById("answer1").checked;
+	var ans1 = document.getElementsByName("one")[0].checked;
 	if(ans1==true){ 
 		percent = percent + 20;
 		ball = ball + 5;
-		alert("1 ok");
 	}
 	 
-    var ans2 = document.getElementById("answer2").checked;
+    var ans2 = document.getElementsByName("two")[2].checked;
 	if (ans2==true){
 		percent = percent + 20;
 		ball = ball + 5;
-		alert("2 ok");
 	}
 	
-	var ans3 = document.getElementById("answer3").checked;
+	var ans3 = document.getElementsByName("three")[3].checked;
 	if (ans3==true){ 
 		percent = percent + 20;
 		ball = ball + 5;
-		alert("3 ok");
 	}
 	
 	var ans4 = document.getElementsByName("four");
@@ -30,7 +27,6 @@ document.getElementById("result").onclick= function checkTest() {
 	if (ans4[0].checked==false && ans4[1].checked==true && ans4[2].checked==false && ans4[3].checked==true){
 	    percent = percent + 20;
 		ball = ball + 5;
-		alert("4 ok");
 	} else if (ans4[0].checked==true && ans4[1].checked==true && ans4[2].checked==false && ans4[3].checked==false){
             percent = percent + 10;
 		    ball = ball + 2.5;
@@ -62,7 +58,6 @@ document.getElementById("result").onclick= function checkTest() {
 	if (ans5[0].checked==true && ans5[1].checked==true && ans5[2].checked==false && ans5[3].checked==false){
 	    percent = percent + 20;
 		ball = ball + 5;
-		alert("5 ok");
 	} else if (ans5[0].checked==true && ans5[1].checked==false && ans5[2].checked==true && ans5[3].checked==false){
             percent = percent + 10;
 		    ball = ball + 2.5;
